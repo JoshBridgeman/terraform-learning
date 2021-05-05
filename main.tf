132c2e8a6f11656b74fbe188a758bcc1e9b3369e
Joshua Bridgeman <jbridgeman.james@gmail.com>
2021-05-05 22:41:15 +0100

    Route Table, IGW, and associate subnet

--- main.tf
+++ main.tf
@@ -22,3 +22,28 @@ resource "aws_subnet" "app-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_route_table" "app-route-table"{
+  vpc_id = aws_vpc.app-vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_internet_gateway" "app-igw" {
+  vpc_id = aws_vpc.app-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table_association" "ass-rtb-subnet" {
+  subnet_id = aws_subnet.app-subnet-1.id
+  route_table_id = aws_route_table.app-route-table.id
+}
