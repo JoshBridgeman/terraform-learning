0e401bde53d6394b3bbb57a865c6abf941e9fb8d
Joshua Bridgeman <jbridgeman.james@gmail.com>
2021-05-08 12:46:32 +0100

    Creates SG, runs latest AWS Linux AMI

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable access_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "app-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -42,3 +44,70 @@ resource "aws_default_route_table" "app-vpc-main-rtb" {
     Name = "${var.env_prefix}-main-rtb"
   }
 }
+
+resource "aws_default_security_group" "default-app-sg" {
+  vpc_id = aws_vpc.app-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.access_ip]
+  }
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "app-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.app-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-app-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "TerraformDemoKeyPair"
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+
+
+output "latest-img" {
+  value = "Image fetched: ${data.aws_ami.latest-amazon-linux-image.name} (${data.aws_ami.latest-amazon-linux-image.id})"
+}
+
+# SSH with ssh -i ~/.ssh/TerraformDemoKeyPair.pem ec2-user@18.135.100.130
